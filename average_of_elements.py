8ab1fe0ea2afe1e6297af46d4f57e5c7eb21d098
Sony Kurian <67534990+sonykurian96@users.noreply.github.com>
2022-08-06 17:13:26 +0530

    Create average_of_elements.py

---
+++ average_of_elements.py
@@ -0,0 +1,14 @@
+'''
+Example 1:
+Input: N = 5, array[] = {1,2,3,4,5}
+Output: 3
+Explanation: Average is the sum of all the elements divided by number of elements.Therefore (1+2+3+4+5)/5 = 3.
+
+Example 2:
+Input:  N=6, array[] = {1,2,1,1,5,1}
+Output: 1.8
+Explanation: Average is the sum of all the elements divided by number of elements.Therefore (1+2+1+1+5+1)/6 = 1.8
+'''
+
+input_list = [int(x) for x in input("Enter list of numbers ").split()]
+print(sum(input_list)/len(input_list))
