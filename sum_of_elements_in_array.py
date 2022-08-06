2a9c7a58401c8d96962a80cf5dacdb0b976a9dc9
Sony Kurian <67534990+sonykurian96@users.noreply.github.com>
2022-08-06 17:08:03 +0530

    Create sum_of_elements_in_array.py

---
+++ sum_of_elements_in_array.py
@@ -0,0 +1,14 @@
+'''
+Example 1:
+Input: N = 5, array[] = {1,2,3,4,5}
+Output: 15
+Explanation: Sum of all the elements is 1+2+3+4+5 = 15
+
+Example 2:
+Input:  N=6, array[] = {1,2,1,1,5,1}
+Output: 11
+Explanation: Sum of all the elements is 1+2+1+1+5+1 = 11
+'''
+
+input_list = [int(x) for x in input("Enter list of numbers ").split()]
+print(sum(input_list))
