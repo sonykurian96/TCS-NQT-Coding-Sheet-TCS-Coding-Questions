8bf2f936ed775a49d6becddcc0b661de4c11ad13
Sony Kurian <67534990+sonykurian96@users.noreply.github.com>
2022-08-06 16:09:30 +0530

    Create reverse_array.py

---
+++ reverse_array.py
@@ -0,0 +1,15 @@
+'''
+Example 1:
+Input: N = 5, arr[] = {5,4,3,2,1}
+Output: {1,2,3,4,5}
+Explanation: Since the order of elements gets reversed the first element will occupy the fifth position, the second element occupies the fourth position and so on.
+
+Example 2:
+Input: N=6 arr[] = {10,20,30,40}
+Output: {40,30,20,10}
+Explanation: Since the order of elements gets reversed the first element will occupy the fifth position, the second element occupies the fourth position and so on.
+'''
+
+input_value = [int(x) for x in input("Enter list of numbers ").split()]
+
+print([int(x) for x in reversed(input_value)])
