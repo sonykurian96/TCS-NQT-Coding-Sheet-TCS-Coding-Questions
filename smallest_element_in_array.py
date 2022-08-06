fa60cf4acf9a5ece5523982257b773632a0e16dc
Sony Kurian <67534990+sonykurian96@users.noreply.github.com>
2022-08-06 14:56:05 +0530

    Create smallest_element_in_array.py

---
+++ smallest_element_in_array.py
@@ -0,0 +1,19 @@
+'''
+Example 1:
+Input: arr[] = {2,5,1,3,0};
+Output: 0
+Explanation: 0 is the smallest element in the array. 
+
+Example2: 
+Input: arr[] = {8,10,5,7,9};
+Output: 5
+Explanation: 5 is the smallest element in the array.
+'''
+
+# soluttion 
+# 1 using inbuilt function
+def minimum(array):
+    return min(array)
+    
+a = [int(x) for x in input("Enter list of numbers ").split()]
+print("minimum value : ",minimum(a))
